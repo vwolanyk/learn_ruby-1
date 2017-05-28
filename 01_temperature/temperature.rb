e0675da65e3b123525c562e46fb9ce5cd1e6b5eb
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-28 10:33:44 -0400

    Temperature Test, add ftoc and ctof methods, tested

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,15 @@
+# Temperature Converter
+
+# Converts Fahrenheit value to Celsius
+def ftoc(fahrenheit)
+
+  (fahrenheit - 32) * 5/9
+
+end
+
+# Converts Celsius to Fahrenheit
+def ctof(celsius)
+
+  (celsius * 9/5) + 32
+
+end
